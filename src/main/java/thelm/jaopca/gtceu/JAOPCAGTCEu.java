5b1e3a307f566614d5ed8577c4c442e38117c5c4
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-02-18 01:23:48 +0800

    Change buildscript to custom loom
    Add logo to mod list entry

--- src/main/java/thelm/jaopca/gtceu/JAOPCAGTCEu.java
+++ src/main/java/thelm/jaopca/gtceu/JAOPCAGTCEu.java
@@ -12,6 +12,6 @@ public class JAOPCAGTCEu {
 
 	public static final String MOD_ID = "jaopcagtceu";
 	public static final String NAME = "JAOPCAGTCEu";
-	public static final String VERSION = "1.12.2-1.0.1.4";
+	public static final String VERSION = "1.12.2-0@VERSION@";
 	public static final String DEPENDENCIES = "required-before:jaopca@[1.12.2-2.3.6,);required-before:gregtech@[2,)";
 }
